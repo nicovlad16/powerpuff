83c1d483ea9a1f4f8f2e62c8bb5ebeed48695bde
Constantin <constantintuca@bitbucket.com>
2019-04-25 10:57:59 +0300

    Home review

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="img/favicon.png" type="image/png">
+        <link rel="icon" href="<?=base_url()?>application/views/img/favicon.png" type="image/png">
         <title>Eventure Multi</title>
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="<?=base_url()?>application/views/css/bootstrap.css">
@@ -26,7 +26,7 @@
         	<nav class="navbar navbar-expand-lg navbar-light">
 				<div class="container box_1620">
 					<!-- Brand and toggle get grouped for better mobile display -->
-					<a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
+					<a class="navbar-brand logo_h" href="<?=base_url()?>"><img src="<?=base_url()?>application/views/img/logo.png" alt=""></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
@@ -35,7 +35,7 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
-							<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
+							<li class="nav-item active"><a class="nav-link" href="<?=base_url()?>">Home</a></li> 
 							<li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li> 
 							<li class="nav-item"><a class="nav-link" href="speakers.html">Speakers</a>
 							<li class="nav-item submenu dropdown">
@@ -54,7 +54,7 @@
 									<li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
 								</ul>
 							</li> 
-							<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+							<li class="nav-item"><a class="nav-link" href="<?=base_url()?>login">Login</a></li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
 							<li class="nav-item"><a href="#" class="tickets_btn">Get Tickets</a></li>
