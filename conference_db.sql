aafdc81f62e1dfbdafda1e2e605e6387f4545326
tudosa_victor <victor.tudosa@gmail.com>
2019-05-23 10:37:06 +0300

    db

--- conference_db.sql
+++ conference_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gazdă: 127.0.0.1
--- Timp de generare: apr. 25, 2019 la 09:56 AM
+-- Timp de generare: mai 23, 2019 la 09:36 AM
 -- Versiune server: 10.1.37-MariaDB
 -- Versiune PHP: 7.3.0
 
@@ -35,6 +35,15 @@ CREATE TABLE `bidding` (
   `pid` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Eliminarea datelor din tabel `bidding`
+--
+
+INSERT INTO `bidding` (`id`, `uid`, `answer`, `pid`) VALUES
+(3, 1, 0, 1),
+(4, 1, 0, 2),
+(6, 6, 0, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -43,14 +52,25 @@ CREATE TABLE `bidding` (
 
 CREATE TABLE `conference` (
   `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `date_start` datetime NOT NULL,
   `date_end` datetime NOT NULL,
   `location` text NOT NULL,
   `no_participants` int(11) NOT NULL,
   `no_speakers` int(11) NOT NULL,
-  `abstract_deadline` datetime NOT NULL,
+  `abstract_deadline` datetime DEFAULT NULL,
+  `bidding_deadline` datetime DEFAULT NULL,
   `paper_deadline` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Eliminarea datelor din tabel `conference`
+--
+
+INSERT INTO `conference` (`id`, `name`, `date_start`, `date_end`, `location`, `no_participants`, `no_speakers`, `abstract_deadline`, `bidding_deadline`, `paper_deadline`) VALUES
+(1, 'Conferinta IT', '2019-05-18 00:00:00', '2019-05-25 00:00:00', 'Str. Mihail Kogalniceanu', 20, 3, '2019-05-20 00:00:00', '2019-05-18 00:00:00', '2019-05-22 00:00:00'),
+(2, 'Dev talk', '2019-05-18 00:00:00', '2019-05-19 00:00:00', 'GG Byron', 0, 0, '2019-05-18 00:00:00', '2019-05-18 00:00:00', '2019-05-18 00:00:00'),
+(3, 'ICCP', '2019-05-18 00:00:00', '2019-05-19 00:00:00', 'FSEGA', 0, 0, '2019-05-18 00:00:00', NULL, '2019-05-18 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -68,8 +88,19 @@ CREATE TABLE `paper` (
   `paper` text,
   `file` varchar(255) DEFAULT NULL,
   `answer` int(11) DEFAULT NULL,
-  `uid` int(11) NOT NULL
+  `uid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Eliminarea datelor din tabel `paper`
+--
+
+INSERT INTO `paper` (`id`, `abstract`, `title`, `keywords`, `topics`, `meta_info`, `paper`, `file`, `answer`, `uid`, `cid`) VALUES
+(1, 'Map Simple abstract', 'Map Simple', 'd', 'Google maps', 'd', '', NULL, NULL, 5, 1),
+(2, 'Scopul conteaza abstract', 'Scopul conteaza', 'Keywords', 'Motivatie', 'Meta info', 'Scopul conteaza paper', NULL, NULL, 5, 1),
+(3, 'Lorem Ipsum dolor sit amet', 'Lorem Ipsum', 'Keywords', 'Lorem Ipsum', 'd', 'Lorem Ipsum dolor sit ametLorem Ipsum dolor sit ametLorem Ipsum dolor sit ametLorem Ipsum dolor sit ametLorem Ipsum dolor sit ametLorem Ipsum dolor sit ametLorem Ipsum dolor sit ametLorem Ipsum dolor sit amet', NULL, NULL, 5, 1),
+(4, 'asdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd asasdasdas asdas dasd as', 'test', 'Keywords', 'Motivatie', 'Meta info', '', NULL, NULL, 7, 3);
 
 -- --------------------------------------------------------
 
@@ -148,7 +179,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `type`, `name`, `email`, `affiliation`, `webpage`) VALUES
-(1, 'chair.boss', '4297f44b13955235245b2497399d7a93', 1, 'Chair Boss', 'chair@powerpuff.ro', NULL, NULL);
+(1, 'chair.boss', '4297f44b13955235245b2497399d7a93', 1, 'Chair Boss', 'chair@powerpuff.ro', NULL, NULL),
+(4, 'author', '4297f44b13955235245b2497399d7a93', 6, 'author', 'contact@webaround.ro', 'aff', NULL),
+(3, 'test', '4297f44b13955235245b2497399d7a93', 3, 'test', 'contact@webaround.ro', 'aff', 'http://localhost/powerpuff/register'),
+(5, 'submitter', '4297f44b13955235245b2497399d7a93', 4, 'submitter', 'submitter@powerpuff.ro', '', NULL),
+(6, 'chair', '4297f44b13955235245b2497399d7a93', 2, 'chair', 'char@test.com', '', ''),
+(7, 'andrei', '4297f44b13955235245b2497399d7a93', 4, 'Andrei', 'admin@admin.com', '', NULL);
 
 --
 -- Indexuri pentru tabele eliminate
@@ -210,19 +246,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pentru tabele `bidding`
 --
 ALTER TABLE `bidding`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pentru tabele `conference`
 --
 ALTER TABLE `conference`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pentru tabele `paper`
 --
 ALTER TABLE `paper`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pentru tabele `presentation`
@@ -252,7 +288,7 @@ ALTER TABLE `session_participant`
 -- AUTO_INCREMENT pentru tabele `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
